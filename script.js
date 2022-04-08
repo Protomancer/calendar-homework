dfcd924e1d363d3cd8c1e1a822f3d75dd7d51a8c
Billy Pinkerton <billybattlebones@gmail.com>
2022-04-08 14:00:00 -0700

    attempting to build the main table

--- script.js
+++ script.js
@@ -5,69 +5,114 @@ let dayPlanner = [
         hour: '09',
         time: '09',
         meridiem: 'am',
-        reminder: '',
+        note: '',
     },
     {
         id: '1',
         hour: '10',
         time: '10',
         meridiem: 'am',
-        reminder: '',
+        note: '',
     },
     {
         id: '2',
         hour: '11',
         time: '11',
         meridiem: 'am',
-        reminder: '',
+        note: '',
     },
     {
         id: '3',
         hour: '12',
         time: '12',
         meridiem: 'pm',
-        reminder: '',
+        note: '',
     },
     {
         id: '4',
         hour: '01',
         time: '13',
         meridiem: 'pm',
-        reminder: '',
+        note: '',
     },
     {
         id: '5',
         hour: '02',
         time: '14',
         meridiem: 'pm',
-        reminder: '',
+        note: '',
     },
     {
         id: '6',
         hour: '03',
         time: '15',
         meridiem: 'pm',
-        reminder: '',
+        note: '',
     },
     {
         id: '7',
         hour: '04',
         time: '16',
         meridiem: 'pm',
-        reminder: '',
+        note: '',
     },
     {
         id: '8',
         hour: '05',
         time: '17',
         meridiem: 'pm',
-        reminder: ''
+        note: ''
     },
     {
         id: '9',
         hour: '06',
         time: '18',
         meridiem: 'pm',
-        reminder: '',
+        note: '',
     }
 ]
+
+/*sets current date and time to header*/
+getDate();
+/*set the style of the columns and rows*/
+dayPlanner.forEach(function(thisHour) {
+    let thisHourRow = $("<form>").attr({
+        "class": "row"
+    });
+    $(".container").append(thisHourRow);
+    /*creates area for current time block.*/
+    let timeSlotArea = $("<div>")
+        .text(`${thisHour.hour}${thisHour.meridiem}`)
+        .attr({
+            "class": "col-md-1 hour"
+    });
+    /*should add schedule data into the text area*/
+    let timeBlockNote = $("<div>")
+        .attr({
+            "class": "col-md-10 description p-0"
+        });
+    let notesData = $("<textarea>");
+    timeBlockNote.append(notesData);
+    notesData.attr("id", thisHour.id);
+    if (thisHour.time < moment().format("HH")) {
+        notesData.attr ({
+            "class": "past", 
+        })
+    } else if (thisHour.time === moment().format("HH")) {
+        notesData.attr({
+            "class": "present"
+        })
+    } else if (thisHour.time > moment().format("HH")) {
+        notesData.attr({
+            "class": "future"
+        })
+    }
+    /*show bootstrap button*/
+    let saveNoteButton = $("<i class='far fa-save fa-lg'></i>")
+    let saveNote = $("<button>")
+        .attr({
+            "class": "col-md-1 saveBtn"
+    });
+    saveNote.append(saveNoteButton);
+    thisHourRow.append(timeSlotArea, timeBlockNote, saveNote);
+})
